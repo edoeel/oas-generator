6fd8dc2f833e64df948767912b667936e231e4ab
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2024-03-03 16:15:19 +0100

    feat: parameters semigroup

---
+++ test/semigroup/parameters.test.ts
@@ -0,0 +1,22 @@
+import {parametersSg} from '@app/semigroup/index';
+import type * as OAS from '@app/oas';
+import {describe, expect, it} from 'bun:test';
+
+const baseParameter: OAS.ParameterObject = {in: 'header', name: 'anHeader'};
+
+describe('Parameters semigroup', () => {
+	it.each([
+		[[baseParameter], [baseParameter], [baseParameter]],
+	])('base parameters', (x, y, expected) => {
+		expect(parametersSg.concat(x, y)).toStrictEqual(expected);
+	});
+	it.each([
+		[[{in: "header", name:"Content-type"}], [{in: "header", name:"Content-type"}], [{in: "header", name:"Content-type"}]],
+		[[{in: "header", name:"Content-type", description: "A description"}], [{in: "header", name:"Content-type"}], [{in: "header", name:"Content-type", description: "A description"}]],
+		[[{in: "header", name:"Content-type"}], [{in: "header", name:"Content-type", description: "A description"}], [{in: "header", name:"Content-type", description: "A description"}]],
+		[[{in: "header", name:"Content-type", description: "A longer description"}], [{in: "header", name:"Content-type", description: "A description"}], [{in: "header", name:"Content-type", description: "A longer description"}]],
+		[[{in: "header", name:"Accept", description: "A longer description"}], [{in: "header", name:"Content-type", description: "A description"}], [{in: "header", name:"Accept", description: "A longer description"}, {in: "header", name:"Content-type", description: "A description"}]],
+	])('should concat parameters merging by in and name properties', (x, y, expected) => {
+		expect(parametersSg.concat(x, y)).toStrictEqual(expected);
+	});
+});
